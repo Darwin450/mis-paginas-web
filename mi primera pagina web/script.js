39a9d20089cf41b81f0cd2bbfdcea7aaebe58d24
Darwin450 <sequeiradarwin316@gmail.com>
2025-05-26 14:03:07 -0600

    Add files via upload

---
+++ mi primera pagina web/script.js
@@ -0,0 +1,4 @@
+document.getElementById('miBoton').addEventListener('click', function() {
+    document.getElementById('mensaje').textContent = '¡Gracias por hacer clic!';
+    console.log('El botón fue clicado.');
+})

6e911f41216dc6414bb8669c877b2fe5d58c85e8
Darwin450 <sequeiradarwin316@gmail.com>
2025-05-26 14:27:13 -0600

    Delete mi primera pagina web directory

--- mi primera pagina web/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('miBoton').addEventListener('click', function() {
-    document.getElementById('mensaje').textContent = '¡Gracias por hacer clic!';
-    console.log('El botón fue clicado.');
-})
